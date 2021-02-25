a7786560d789462b626541ff1392afee51470663
Robert <robert.czwartosz@tlen.pl>
2021-02-25 19:41:02 +0100

    add service, controller and routes to container

--- Api/CreateContainer.ts
+++ Api/CreateContainer.ts
@@ -6,33 +6,47 @@ import App from './App';
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import 'dotenv/config';
+
 import UserModel from './Src/Models/User';
 import UserRepository from './Src/Repositories/User';
+import UserService from './Src/Services/User';
+import UserController from './Src/Controllers/UserController';
+import userRoutes from './Src/Routes/User';
 
 const appContainer = new Container();
+
 // Mongo config
 appContainer.declare("Port", (c) => process.env.PORT);
 appContainer.declare("MongoUrl", (c) => process.env.MONGO_URL);
+
 // Middlewares
 const middlewares = [
   bodyParser.json()
 ];
 appContainer.declare("Middlewares", (c) => middlewares);
+
 // Models
 appContainer.declare('UserModel', (c) => UserModel);
+
 // Repositories
 appContainer.declare('UserRepository', (c) => new UserRepository(c.UserModel));
+
 // Services
-appContainer.declare("SampleService", (c) => new SampleService());
+appContainer.declare("UserService", (c) => new UserService(c.UserRepository));
+
 // Controllers
-appContainer.declare("SampleController", (c) => new SampleController(c.SampleService));
+appContainer.declare("UserController", (c) => new UserController(c.UserService));
+
 // Routes
 appContainer.declare("Routes", (c) => [
-  sampleRoutes(c.SampleController)
+  userRoutes(c.UserController)
 ]);
+
 // Create router
 type Routes = (router: express.Router) => express.Router;
 appContainer.declare( "Router", (c) =>  c.Routes.reduce( (router: express.Router, addRoutes: Routes) => addRoutes(router), express.Router() ) );
+
+// Create App
 appContainer.declare(
   "App",
   (c) =>
