cf8a7a70f4ae481f578ea358d4238c7c29387f13
Robert <robert.czwartosz@tlen.pl>
2021-02-25 19:39:59 +0100

    implement user controller

---
+++ Api/Src/Controllers/UserController.ts
@@ -0,0 +1,25 @@
+import { UserModel } from '../Models/User';
+import UserService from '../Services/User';
+import { Request, Response } from 'express';
+import * as mongoose from 'mongoose';
+
+
+export default class UserController {
+    service: UserService;
+    constructor(service: UserService) {
+        this.service = service;
+    }
+
+    getUser = async (req: Request, res: Response) => {
+        const id = new mongoose.Schema.Types.ObjectId(req.params.id);
+        const user = await this.service.findUserById(id);
+        if(!user) return res.status(404).json({message: 'User not found'});
+        res.status(200).json(user);
+    }
+    
+    register = async (req: Request, res: Response) => {
+        const user: UserModel = req.body;
+        await this.service.createUser(user);
+        res.status(201).json({message: 'Register succeed'});
+    }
+}
