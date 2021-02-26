b127e6fa5eb6e8ae8f37bb4fda9c1c83b8f332ac
Konrad <kj.reczek@gmail.com>
2021-02-26 19:57:31 +0100

    Created project routes

--- Api/Src/Routes/ProjectRoutes.ts
+++ Api/Src/Routes/ProjectRoutes.ts
@@ -3,8 +3,9 @@ import ProjectController from "../Controllers/ProjectController";
 
 const projectRoutes = (controller: ProjectController) => {
     return (router: express.Router) => {
-        router.route("/projects").get(controller.XXXXXXXXX);
-        router.route("/projects/:id").get(controller.XXXXXXX);
+        router.route("/projects").get(controller.getAllProjects);
+        router.route("/projects/:id").get(controller.getProject);
+        router.route("/projects").get(controller.createProject);
         return router;
     }
 
