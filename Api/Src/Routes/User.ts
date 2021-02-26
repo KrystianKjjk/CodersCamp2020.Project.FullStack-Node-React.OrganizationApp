fc20778c368243465c10289554915313891c7869
Robert <robert.czwartosz@tlen.pl>
2021-02-26 19:38:58 +0100

    add controllers and routes

--- Api/Src/Routes/User.ts
+++ Api/Src/Routes/User.ts
@@ -5,7 +5,11 @@ import * as express from 'express';
 export default function UserRoutes(c: UserController) {
     return (router: express.Router) => {
         router.get('/users/:id', c.getUser);
-        router.post('/users', c.register);
+        router.get('/users', c.getAllUsers);
+        router.post('/users/register', c.register);
+        router.post('/users/login', c.logIn);
+        router.patch('/users/:id', c.updateUser);
+        router.delete('/users/:id', c.deleteUser);
         return router;
     }
 }
