bf0d56daa19582bc8d9a4f27076c1876f95beeac
ireneusz <ireneusz.petryszak@gmail.com>
2021-02-28 19:03:36 +0100

    fix

--- Api/Test/AuthService.test.ts
+++ Api/Test/AuthService.test.ts
@@ -5,11 +5,11 @@ import { Document, Types } from 'mongoose';
 import User from '../Src/Models/User';
 import * as mongoose from 'mongoose';
 
-type User = UserModel & mongoose.Document;
+type UserType = UserModel & mongoose.Document;
 process.env.JWT_PRIVATE_KEY = 'thisisprivatekey';
 
 class TestRepository extends UserRepository {
-    users: Array<UserModel & Document>;
+    users: Array<UserType>;
 
     constructor() {
         super(User);
@@ -40,20 +40,20 @@ describe('Test AuthService ', () => {
     };
 
     (async () => {
-        const password = await service.hashPassword(user as User);
+        const password = await service.hashPassword(user as UserType);
         let user2 = Object.assign({}, user);
         user2.password = password;
-        await service.saveUser(user2 as User);
+        await service.saveUser(user2 as UserType);
     })();
 
     test(`generate token`, () => {
-        expect(typeof service.generateToken(user as User)).toBe('string');
+        expect(typeof service.generateToken(user as UserType)).toBe('string');
     });
     test(`find user`, async () => {
         expect(await service.findUser(`test@test.pl`)).toBe(repo.users[0]);
     });
     test(`hash password`, async () => {
-        expect(typeof await service.hashPassword(user as User)).toBe('string');
+        expect(typeof await service.hashPassword(user as UserType)).toBe('string');
     });
     test(`check password`, async () => {
         expect(await service.checkPassword(user.password, repo.users[0])).toBe(true);
