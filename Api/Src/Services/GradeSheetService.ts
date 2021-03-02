24bce8e9cfb57e5e09be992e49d6aeed7e95447f
Robert <robert.czwartosz@tlen.pl>
2021-03-02 16:22:58 +0100

    refactor

--- Api/Src/Services/GradeSheetService.ts
+++ Api/Src/Services/GradeSheetService.ts
@@ -33,32 +33,15 @@ export default class UserService {
     }
 
     async addMentorReviewer(gradeSheetId: mongoose.Types.ObjectId, mentorId: mongoose.Types.ObjectId) {
-        const updateQuery = {
-            $push: mentorId
-        }
-        return await this.repository.updateById(gradeSheetId, updateQuery);
+        return await this.repository.addMentorReviewer(gradeSheetId, mentorId);
     }
 
     async setMentorGrade(gradeSheetId: mongoose.Types.ObjectId, gradeName: string, grade: number) {
-        const updateQuery = {
-            $set: {
-                mentorGrade: { }
-            }
-        }
-        updateQuery.$set.mentorGrade[gradeName] = grade;
-        return await this.repository.updateById(gradeSheetId, updateQuery);
+        return await this.repository.setMentorGrade(gradeSheetId, gradeName, grade);
     }
 
     async setMentorReviewerGrade(gradeSheetId: mongoose.Types.ObjectId, mentorId: mongoose.Types.ObjectId, gradeName: string, grade: number) {
-        const updateQuery = {
-            $set: {
-                mentorReviewerGrades: { }
-            }
-        }
-        const sheet = await this.repository.getById(gradeSheetId) as GradeSheet;
-        const index = sheet.mentorReviewerGrades.findIndex(grade => grade.mentor === mentorId);
-        updateQuery.$set.mentorReviewerGrades[index].grades[gradeName] = grade;
-        return await this.repository.updateById(gradeSheetId, updateQuery);
+        return await this.repository.setMentorReviewerGrade(gradeSheetId, mentorId, gradeName, grade);
     }
 
     async deleteUser(id: mongoose.Types.ObjectId) {
