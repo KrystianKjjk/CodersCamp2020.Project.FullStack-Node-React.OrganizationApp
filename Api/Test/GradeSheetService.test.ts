b7f6a70873937ea0023d8ca7ff666d37236e63cb
Robert <robert.czwartosz@tlen.pl>
2021-03-02 19:54:56 +0100

    add tests

--- Api/Test/GradeSheetService.test.ts
+++ Api/Test/GradeSheetService.test.ts
@@ -38,6 +38,28 @@ class TestRepository extends GradeSheetRepository {
         this.gradeSheets = this.gradeSheets.filter(sheet => sheet._id !== id)
     };
 
+    async addMentorReviewer(gradeSheetId: Types.ObjectId, mentorId: Types.ObjectId) {
+        const gradeSheet = await this.getById(gradeSheetId);
+        const reviewers = [...gradeSheet.mentorReviewer, mentorId];
+        this.updateById(gradeSheetId, {mentorReviewer: reviewers});
+    }
+
+    async setMentorReviewers(gradeSheetId: Types.ObjectId, mentorIds: Types.ObjectId[]) {
+        this.updateById(gradeSheetId, {mentorReviewer: mentorIds});
+    }
+
+    async setMentorGrade(gradeSheetId: Types.ObjectId, gradeName: string, grade: number) {
+        const index = this.gradeSheets.findIndex(sheet => sheet._id === gradeSheetId);
+        this.gradeSheets[index].mentorGrades[gradeName] = grade;
+    }
+
+    async setMentorReviewerGrade(gradeSheetId: Types.ObjectId, mentorId: Types.ObjectId, gradeName: string, grade: number) {
+        const index = this.gradeSheets.findIndex(sheet => sheet._id === gradeSheetId);
+        const reviewerGrades = this.gradeSheets[index].mentorReviewerGrades;
+        const reviewerIndex = reviewerGrades.findIndex(grades => grades.mentor === mentorId);
+        reviewerGrades[reviewerIndex].grades[gradeName] = grade;
+    }
+
 }
 
 const testRepo = new TestRepository()
@@ -98,8 +120,41 @@ describe('Test GradeSheetService ', () => {
         expect(await service.findGradeSheetById(gradeSheets[0]._id)).toEqual(gradeSheets[0]);
     });
 
-    test('update sheet', () => {
-        expect(2+2).toBe(4);
+    test('add mentor reviewer', async () => {
+        const idx = 0;
+        const sheetId = gradeSheets[idx]._id;
+        const mentorId = new Types.ObjectId();
+        expect(gradeSheets[idx].mentorReviewer).toHaveLength(0);
+        await service.addMentorReviewer(sheetId, mentorId);
+        expect(gradeSheets[idx].mentorReviewer).toHaveLength(1);
+    });
+
+    test('set mentor reviewers', async () => {
+        const idx = 1;
+        const sheetId = gradeSheets[idx]._id;
+        const mentorIds = [];
+        expect(gradeSheets[idx].mentorReviewer).toHaveLength(1);
+        await service.setMentorReviewers(sheetId, mentorIds);
+        expect(gradeSheets[idx].mentorReviewer).toHaveLength(0);
+    });
+
+    test('set mentor grade', async () => {
+        const idx = 7;
+        const sheetId = gradeSheets[idx]._id;
+        const gradeName = 'ExtraGrade';
+        const grade = 111;
+        await service.setMentorGrade(sheetId, gradeName, grade);
+        expect(gradeSheets[idx].mentorGrades[gradeName]).toBe(grade);
+    });
+
+    test('set mentor reviewer grade', async () => {
+        const idx = 7;
+        const sheetId = gradeSheets[idx]._id;
+        const mentorId = gradeSheets[idx].mentorReviewer[0];
+        const gradeName = 'ExtraRevGrade';
+        const grade = 321;
+        await service.setMentorReviewerGrade(sheetId, mentorId, gradeName, grade);
+        expect(gradeSheets[idx].mentorReviewerGrades[0].grades[gradeName]).toBe(grade);
     });
 
     test('delete sheet', async () => {
