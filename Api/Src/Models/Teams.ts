13ae6d16108cfeacabbe0152bc271c42a14a231a
Konrad <kj.reczek@gmail.com>
2021-03-03 20:02:17 +0100

    Created teams model

--- Api/Src/Models/Teams.ts
+++ Api/Src/Models/Teams.ts
@@ -1,11 +1,24 @@
 import * as mongoose from 'mongoose';
 
 export interface Teams {
-
+    mentor: mongoose.Types.ObjectId;
+    users: mongoose.Types.ObjectId[];
+    projects: mongoose.Types.ObjectId[];
 };
 
 const TeamsSchema = new mongoose.Schema({
-    
+    mentor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    users: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    }],
+    projects: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Project',
+    }],
 
   }, {timestamps: true});
 
