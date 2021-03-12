b47e8617aec872faeacc7f18acab99597d88ba79
Konrad <kj.reczek@gmail.com>
2021-03-12 17:56:11 +0100

    Added routes to section

--- Api/Src/Routes/SectionRoutes.ts
+++ Api/Src/Routes/SectionRoutes.ts
@@ -1,13 +1,18 @@
 import * as express from 'express';
 import SectionController from '../Controllers/SectionController';
+import TestController from '../Controllers/TestController';
 
-const sectionRoutes = (controller: SectionController) => {
+const sectionRoutes = (sectionController: SectionController, testController: TestController) => {
     return (router: express.Router) => {
-        router.route("/sections").get(controller.getSections);
-        router.route("/sections/:id").get(controller.getSectionById);
-        router.route("/sections").post(controller.createSection);
-        router.route("/sections/:id").put(controller.updateSection);
-        router.route("/sections/:id").delete(controller.deleteSection);
+        router.route("/sections").get(sectionController.getSections);
+        router.route("/sections/:id").get(sectionController.getSectionById);
+        router.route("/sections").post(sectionController.createSection);
+        router.route("/sections/:id").put(sectionController.updateSection);
+        router.route("/sections/:id").delete(sectionController.deleteSection);
+        
+        router.route("/sections/:id/test").post(testController.addTest);
+        router.route("/sections/:sectionId/test/:testId").patch(testController.updateTest);
+        router.route("/sections/:sectionId/test/:testId").delete(testController.deleteTest);
         return router;
     }
 };
