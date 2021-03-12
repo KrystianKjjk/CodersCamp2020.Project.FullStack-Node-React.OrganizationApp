d4a2b8a4425ec7d7944974ccd76f5ecd1a963bff
Konrad <kj.reczek@gmail.com>
2021-03-12 17:55:30 +0100

    Updated test controller

--- Api/Src/Controllers/TestController.ts
+++ Api/Src/Controllers/TestController.ts
@@ -1,27 +1,41 @@
 import { Request, Response } from "express";
 import * as mongoose from 'mongoose';
 import TestService from '../Services/TestService';
+import { Test } from '../Models/Section';
+import SectionService from "../Services/SectionService";
 
 export default class TestController {
-    service: TestService;
-    constructor(service: TestService) {
-        this.service = service;
+    testService: TestService;
+    sectionService: SectionService;
+    constructor(testService: TestService, sectionService: SectionService) {
+        this.testService = testService;
+        this.sectionService = sectionService;
     }
 
     addTest = async (req: Request, res: Response) => {
         const sectionId = new mongoose.Types.ObjectId(req.params.id);
-        const object = new mongoose.Types.ObjectId(req.body.test);
-        const section = await this.service.addTest(sectionId, object);
+        const test = req.body;
+        const section = await this.testService.addTest(sectionId, test);
         if(!section) return res.status(404).json({message: 'Section not found'});
         res.status(200).json({message: 'Test added to section'});
     }
 
+    updateTest = async (req: Request, res: Response) => {
+        const sectionId = new mongoose.Types.ObjectId(req.params.sectionId);
+        const testId = new mongoose.Types.ObjectId(req.params.testId);
+        const test = req.body;
+        const section = await this.sectionService.getSectionById(sectionId);
+        if (!section) res.status(404).json({message: 'Section not found'});
+        await this.testService.updateTest(sectionId, testId, test);
+        res.status(200).json({message: 'Test was updated'});
+    }
+
     deleteTest = async (req: Request, res: Response) => {
         const sectionId = new mongoose.Types.ObjectId(req.params.sectionId);
-        const testId = new mongoose.Types.ObjectId(req.params.userId);
-        const section = await this.service.getSectionById(sectionId);
+        const testId = new mongoose.Types.ObjectId(req.params.testId);
+        const section = await this.sectionService.getSectionById(sectionId);
         if (!section) res.status(404).json({message: 'Section not found'});
-        await this.service.deleteTest(sectionId, testId);
+        await this.testService.deleteTest(sectionId, testId);
         res.status(200).json({message: 'Test was deleted'});
     }
 }
