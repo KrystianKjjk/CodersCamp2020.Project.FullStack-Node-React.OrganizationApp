64c9c0faf08418c59af4005d0e0bff00b91a965b
Robert <robert.czwartosz@tlen.pl>
2021-03-12 22:01:07 +0100

    add auth to user routes

--- Api/Src/Routes/User.ts
+++ Api/Src/Routes/User.ts
@@ -1,15 +1,16 @@
 import UserController from '../Controllers/User';
 import * as express from 'express';
 import idValidation from '../Middlewares/IdValidation'
+import { UserType } from '../Models/User';
+import { HasRole } from '../Middlewares/HasRole';
 
 export default function UserRoutes(c: UserController) {
     return (router: express.Router) => {
-        router.get('/users/:id', idValidation, c.getUser);
-        router.get('/users', c.getAllUsers);
-        router.post('/users/register', c.register);
-        router.post('/users/login', c.logIn);
-        router.patch('/users/:id', idValidation, c.updateUser);
-        router.delete('/users/:id', idValidation, c.deleteUser);
+        router.get('/users/:id', HasRole([UserType.Admin]), idValidation, c.getUser);
+        router.get('/users', HasRole([UserType.Admin]), c.getAllUsers);
+        router.post('/users/register', HasRole([UserType.Admin]), c.register);
+        router.patch('/users/:id', HasRole([UserType.Admin]), idValidation, c.updateUser);
+        router.delete('/users/:id', HasRole([UserType.Admin]), idValidation, c.deleteUser);
         return router;
     }
 }
