d155accb1831f334e84841b327910fd1acbd2e09
Robert <robert.czwartosz@tlen.pl>
2021-03-12 21:57:12 +0100

    add auth to section routes

--- Api/Src/Routes/SectionRoutes.ts
+++ Api/Src/Routes/SectionRoutes.ts
@@ -1,13 +1,15 @@
 import * as express from 'express';
 import SectionController from '../Controllers/SectionController';
+import { HasRole } from '../Middlewares/HasRole';
+import { UserType } from '../Models/User';
 
 const sectionRoutes = (controller: SectionController) => {
     return (router: express.Router) => {
-        router.route("/sections").get(controller.getSections);
-        router.route("/sections/:id").get(controller.getSectionById);
-        router.route("/sections").post(controller.createSection);
-        router.route("/sections/:id").put(controller.updateSection);
-        router.route("/sections/:id").delete(controller.deleteSection);
+        router.route("/sections").get(HasRole([UserType.Admin, UserType.Mentor, UserType.Participant]), controller.getSections);
+        router.route("/sections/:id").get(HasRole([UserType.Admin, UserType.Mentor, UserType.Participant]), controller.getSectionById);
+        router.route("/sections").post(HasRole([UserType.Admin]), controller.createSection);
+        router.route("/sections/:id").put(HasRole([UserType.Admin]), controller.updateSection);
+        router.route("/sections/:id").delete(HasRole([UserType.Admin]), controller.deleteSection);
         return router;
     }
 };
