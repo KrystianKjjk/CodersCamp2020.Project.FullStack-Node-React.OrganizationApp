29be3838a8b0f5684cce9224c8c51d62435feb15
Robert <robert.czwartosz@tlen.pl>
2021-03-12 21:50:52 +0100

    add fix

--- Api/Src/Routes/ProjectRoutes.ts
+++ Api/Src/Routes/ProjectRoutes.ts
@@ -6,7 +6,7 @@ import { UserType } from "../Models/User";
 
 const projectRoutes = (controller: ProjectController) => {
     return (router: express.Router) => {
-        router.route("/projects").get(HasRole([UserType.Admin]), controller.getAllProjects);
+        router.route("/projects").get(HasRole([UserType.Admin, UserType.Mentor, UserType.Participant]), controller.getAllProjects);
         router.route("/projects/:id").get(HasRole([UserType.Admin, UserType.Mentor, UserType.Participant]), idValidation, controller.getProject);
         router.route("/projects").post(HasRole([UserType.Admin]), controller.createProject);
         router.route("/projects/:id").patch(HasRole([UserType.Admin]), idValidation, controller.updateProject);
