c6d8f8b758ce744cf84374d9736e37ff3ff70c47
Robert <robert.czwartosz@tlen.pl>
2021-03-13 07:31:30 +0100

    add auth to team routes

--- Api/Src/Routes/TeamProjectRoute.ts
+++ Api/Src/Routes/TeamProjectRoute.ts
@@ -1,13 +1,15 @@
 import * as express from 'express';
 import TeamProjectController from '../Controllers/TeamProjectController';
+import { HasRole } from '../Middlewares/HasRole';
+import { UserType } from '../Models/User';
 
 const courseRoute = (controller: TeamProjectController) => {
     return (router: express.Router) => {
-        router.route("/teams/projects").get(controller.getTeamProjects);
-        router.route("/teams/projects/:id").get(controller.getTeamProjectById);
-        router.route("/teams/projects").post(controller.createTeamProject);
-        router.route("/teams/projects/:id").put(controller.updateTeamProject);
-        router.route("/teams/projects/:id").delete(controller.deleteTeamProject);
+        router.route("/teams/projects").get(HasRole([UserType.Admin]), controller.getTeamProjects);
+        router.route("/teams/projects/:id").get(HasRole([UserType.Admin]), controller.getTeamProjectById);
+        router.route("/teams/projects").post(HasRole([UserType.Admin]), controller.createTeamProject);
+        router.route("/teams/projects/:id").put(HasRole([UserType.Admin]), controller.updateTeamProject);
+        router.route("/teams/projects/:id").delete(HasRole([UserType.Admin]), controller.deleteTeamProject);
         return router;
     }
 };
