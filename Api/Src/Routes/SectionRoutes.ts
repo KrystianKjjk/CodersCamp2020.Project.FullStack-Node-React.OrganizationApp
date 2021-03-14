03e8ed9933de46c579038db26f26ff36c9ca022d
Robert <robert.czwartosz@tlen.pl>
2021-03-14 12:30:38 +0100

    correct section endpoints auth

--- Api/Src/Routes/SectionRoutes.ts
+++ Api/Src/Routes/SectionRoutes.ts
@@ -5,12 +5,12 @@ import { UserType } from '../Models/User';
 
 const sectionRoutes = (controller: SectionController) => {
     return (router: express.Router) => {
-        router.route("/sections").get(HasRole([UserType.Admin, UserType.Mentor, UserType.Participant]), controller.getSections);
-        router.route("/sections/:id").get(HasRole([UserType.Admin, UserType.Mentor, UserType.Participant]), controller.getSectionById);
+        router.route("/sections").get(HasRole([UserType.Admin]), controller.getSections);
+        router.route("/sections/:id").get(controller.getSectionById);
         router.route("/sections").post(HasRole([UserType.Admin]), controller.createSection);
         router.route("/sections/:id").put(HasRole([UserType.Admin]), controller.updateSection);
         router.route("/sections/:id").delete(HasRole([UserType.Admin]), controller.deleteSection);
-        router.route("/courses/:id/sections").get(HasRole([UserType.Admin]), controller.getSectionsByCourseId);
+        router.route("/courses/:id/sections").get(controller.getSectionsByCourseId);
         return router;
     }
 };
