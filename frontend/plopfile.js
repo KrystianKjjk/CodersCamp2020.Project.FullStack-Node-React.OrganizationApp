3de21d7b7166300530423b1e90998348c216a3af
Konrad <kj.reczek@gmail.com>
2021-03-21 16:25:06 +0100

    Change template for component

--- frontend/plopfile.js
+++ frontend/plopfile.js
@@ -17,63 +17,25 @@ module.exports = plop => {
           // Add a new file
           type: 'add',
           // Path for the new file
-          path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+          path: './{{pascalCase name}}/{{pascalCase name}}.tsx',
           // Handlebars template used to generate content of new file
           templateFile: 'src/plop-templates/Component/Component.hbs',
         },
         {
             type: 'add',
-            path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx',
+            path: './{{pascalCase name}}/{{pascalCase name}}.test.ts',
             templateFile: 'src/plop-templates/Component/Component.test.hbs',
         },
         {
             type: 'add',
-            path: 'src/components/{{pascalCase name}}/index.tsx',
+            path: './{{pascalCase name}}/index.ts',
             templateFile: 'src/plop-templates/Component/index.hbs',
         },
         {
             type: 'add',
-            path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.css',
+            path: './{{pascalCase name}}/{{pascalCase name}}.css',
             templateFile: 'src/plop-templates/Component/Component.css.hbs',
         },
-      ],
-    });
-
-    plop.setGenerator('component-redux', {
-        description: 'Create a component redux',
-        prompts: [
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your component name?'
-        },
-      ],
-        actions: [
-        {
-          type: 'add',
-          path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
-          templateFile: 'src/plop-templates/ComponentRedux/Component.hbs',
-        },
-        {
-            type: 'add',
-            path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx',
-            templateFile: 'src/plop-templates/ComponentRedux/Component.test.hbs',
-        },
-        {
-            type: 'add',
-            path: 'src/components/{{pascalCase name}}/index.tsx',
-            templateFile: 'src/plop-templates/ComponentRedux/index.hbs',
-        },
-        {
-            type: 'add',
-            path: 'src/components/{{pascalCase name}}/index.tsx',
-            templateFile: 'src/plop-templates/ComponentRedux/index.hbs',
-        },
-        {
-            type: 'add',
-            path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.css',
-            templateFile: 'src/plop-templates/ComponentRedux/Component.css.hbs',
-        },
-      ],
-    });
-  };
+    ],
+  });
+};
