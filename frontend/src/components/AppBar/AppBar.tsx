2723fef1211f73f9955886e612d2966163d4dfd9
Konrad <kj.reczek@gmail.com>
2021-03-24 21:24:00 +0100

    change appbar component

--- frontend/src/components/AppBar/AppBar.tsx
+++ frontend/src/components/AppBar/AppBar.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-
 export interface AppBarProps {
 
 };
@@ -20,6 +16,12 @@ const useStyles = makeStyles((theme) => ({
 
   blackBackground: {
     backgroundColor: '#292929'
+  },
+
+  imageCoders: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    height: 24
   }
 }));
 
@@ -30,7 +32,7 @@ export default function DenseAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar variant="dense" className={classes.blackBackground}>
-          <img src='../img/coderscrew.png' height="50px"></img>
+          <img src='../img/coderscrew.png' className={classes.imageCoders} alt=''/>
         </Toolbar>
       </AppBar>
     </div>
