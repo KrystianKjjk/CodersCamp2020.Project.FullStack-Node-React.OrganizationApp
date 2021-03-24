e1abf7ef92c6a6cc77b7cfe028c24e4eb8491479
Konrad <kj.reczek@gmail.com>
2021-03-24 17:12:30 +0100

    create registration component

--- frontend/src/components/Registration/Registration.tsx
+++ frontend/src/components/Registration/Registration.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
+
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -25,16 +21,29 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     alignItems: 'center',
   },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.primary.main,
-  },
   form: {
     width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(3),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
+  },
+
+  cssLabel: {
+    color : '#FFF !important'
+  },
+
+  cssFocused: { },
+
+  notchedOutline: {
+    borderWidth: '1px',
+    borderColor: '#696969'
+  },
+
+  outlinedInput: {
+    '&$cssFocused $notchedOutline': {
+      border: '1px solid #FFF'
+    },
   },
 }));
 
@@ -61,6 +70,19 @@ export default function SignUp() {
                 id="firstName"
                 label="First Name"
                 autoFocus
+                InputLabelProps={{
+                  classes: {
+                    root: classes.cssLabel,
+                    focused: classes.cssLabel
+                  },
+                }}
+                InputProps={{
+                  classes: {
+                    root: classes.outlinedInput,
+                    focused: classes.cssFocused,
+                    notchedOutline: classes.notchedOutline
+                  },
+                }}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
@@ -72,6 +94,19 @@ export default function SignUp() {
                 label="Last Name"
                 name="lastName"
                 autoComplete="lname"
+                InputLabelProps={{
+                  classes: {
+                    root: classes.cssLabel,
+                    focused: classes.cssLabel
+                  },
+                }}
+                InputProps={{
+                  classes: {
+                    root: classes.outlinedInput,
+                    focused: classes.cssFocused,
+                    notchedOutline: classes.notchedOutline
+                  },
+                }}
               />
             </Grid>
             <Grid item xs={12}>
@@ -83,6 +118,19 @@ export default function SignUp() {
                 label="Email Address"
                 name="email"
                 autoComplete="email"
+                InputLabelProps={{
+                  classes: {
+                    root: classes.cssLabel,
+                    focused: classes.cssLabel
+                  },
+                }}
+                InputProps={{
+                  classes: {
+                    root: classes.outlinedInput,
+                    focused: classes.cssFocused,
+                    notchedOutline: classes.notchedOutline
+                  },
+                }}
               />
             </Grid>
             <Grid item xs={12}>
@@ -95,6 +143,19 @@ export default function SignUp() {
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                InputLabelProps={{
+                  classes: {
+                    root: classes.cssLabel,
+                    focused: classes.cssLabel
+                  },
+                }}
+                InputProps={{
+                  classes: {
+                    root: classes.outlinedInput,
+                    focused: classes.cssFocused,
+                    notchedOutline: classes.notchedOutline
+                  },
+                }}
               />
             </Grid>
             <Grid item xs={12}>
@@ -107,6 +168,19 @@ export default function SignUp() {
                 type="passwordconfirm"
                 id="passwordconfirm"
                 autoComplete="current-password"
+                InputLabelProps={{
+                  classes: {
+                    root: classes.cssLabel,
+                    focused: classes.cssLabel
+                  },
+                }}
+                InputProps={{
+                  classes: {
+                    root: classes.outlinedInput,
+                    focused: classes.cssFocused,
+                    notchedOutline: classes.notchedOutline
+                  },
+                }}
               />
             </Grid>
           </Grid>
@@ -121,7 +195,7 @@ export default function SignUp() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="#" variant="body2" color="inherit">
                 Already have an account? Sign in
               </Link>
             </Grid>
