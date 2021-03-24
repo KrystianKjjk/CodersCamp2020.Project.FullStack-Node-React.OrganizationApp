9bca927c2b30b4aa533b5dc6ceee216f83423430
Konrad <kj.reczek@gmail.com>
2021-03-24 17:14:34 +0100

    create login component

--- frontend/src/components/LogIn/LogIn.tsx
+++ frontend/src/components/LogIn/LogIn.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -7,28 +6,13 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
 export interface LogInProps {
 
 };
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -37,21 +21,47 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     alignItems: 'center',
   },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
+  
   form: {
     width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
+
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
+
   body: {
     backgroundColor: '#292929',
   },
+
+  cssLabel: {
+    color : '#FFF !important'
+  },
+
+  cssFocused: { },
+
+  notchedOutline: {
+    borderWidth: '1px',
+    borderColor: '#696969'
+  },
+
+  outlinedInput: {
+    '&$cssFocused $notchedOutline': {
+      border: '1px solid #FFF'
+    },
+  },
 }));
+
+const GreenCheckbox = withStyles({
+  root: {
+    color: 'white',
+    '&$checked': {
+      color: 'white',
+    },
+  },
+  checked: {},
+})((props: any) => <Checkbox color="default" {...props} />);
 
 export default function SignIn() {
   const classes = useStyles();
@@ -60,9 +70,6 @@ export default function SignIn() {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
@@ -77,6 +84,19 @@ export default function SignIn() {
             name="email"
             autoComplete="email"
             autoFocus
+            InputLabelProps={{
+              classes: {
+                root: classes.cssLabel,
+                focused: classes.cssLabel
+              },
+            }}
+            InputProps={{
+              classes: {
+                root: classes.outlinedInput,
+                focused: classes.cssFocused,
+                notchedOutline: classes.notchedOutline
+              },
+            }}
           />
           <TextField
             variant="outlined"
@@ -88,9 +108,22 @@ export default function SignIn() {
             type="password"
             id="password"
             autoComplete="current-password"
+            InputLabelProps={{
+              classes: {
+                root: classes.cssLabel,
+                focused: classes.cssLabel
+              },
+            }}
+            InputProps={{
+              classes: {
+                root: classes.outlinedInput,
+                focused: classes.cssFocused,
+                notchedOutline: classes.notchedOutline
+              },
+            }}
           />
           <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
+            control={<GreenCheckbox value="remember" color="primary" />}
             label="Remember me"
           />
           <Button
@@ -104,21 +137,18 @@ export default function SignIn() {
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link href="#" variant="body2">
+              <Link href="#" variant="body2" color="inherit">
                 Forgot password?
               </Link>
             </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="#" variant="body2" color="inherit">
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
           </Grid>
         </form>
       </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
     </Container>
   );
 }
