9c546cc86150ba20ed4a0c866a1efa9ec9989339
robert-czwartosz <robert.czwartosz@tlen.pl>
2021-03-24 08:27:19 +0100

    provide redux store to storybook

--- frontend/src/components/ReusableTable/ReusableTable.stories.tsx
+++ frontend/src/components/ReusableTable/ReusableTable.stories.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import ReusableTable , { ReusableTableProps } from './ReusableTable';
+import { store } from '../../app/store';
+import { Provider } from 'react-redux';
 
 export default {
   title: 'ReusableTable component',
   component: ReusableTable,
 } as Meta;
 
-const Template: Story<ReusableTableProps> = (args) => <ReusableTable {...args} />;
+const Template: Story<ReusableTableProps> = (args) => (
+  <Provider store={store}>
+    <ReusableTable {...args} />
+  </Provider>
+);
 
 export const SampleReusableTable = Template.bind({});
 SampleReusableTable.args = {
