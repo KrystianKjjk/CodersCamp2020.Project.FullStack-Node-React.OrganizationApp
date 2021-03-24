17fcee143b1d48ee37cfd3814ea24a7cffe90106
FIlip <filip.szwedo@gmail.com>
2021-03-24 20:45:18 +0100

    basic visual setup finished

--- frontend/src/components/TeamProject/TeamProject.tsx
+++ frontend/src/components/TeamProject/TeamProject.tsx
@@ -3,36 +3,31 @@ import styles from './TeamProject.module.css'
 import Button from '@material-ui/core/Button';
 
 export interface TeamProjectProps {
-  _id: string,
-  teamId: string,
-  parentProjectIds: string,
-  projectName: string,
-  projectUrl: string,
-  description: string
+  _id: string //id projektu wybranego w tabeli
 }
 
 const TeamProject: React.FC< TeamProjectProps > = props => {
-  
+
 
 
   return (
     <div className={styles.teamProjectContainer}>
-      <div className="team-project-header">
-        <span>HEADER</span>
-        <Button className={styles.button}>Edit</Button>
-        <Button className="button">Delete</Button>
+      <div className={styles.teamProjectHeader}>
+        <span className={styles.teamProjectHeaderName}>Manage team project</span>
+        <Button className={styles.buttonDelete}>Delete</Button>
+        <Button className={styles.buttonEdit}>Edit</Button>
       </div>
 
-      <div className={styles.teamDetailsContainer}>
-      <div className="attribute-names-container">
-        <div >Name:</div>
+      <div className={styles.teamProjectDetailsContainer}>
+      <div className={styles.attributeNamesContainer}>
+        <div>Name:</div>
         <div>Reference project:</div>
         <div>Team mentor:</div>
         <div>Section name:</div>
         <div>Project URL:</div>
         <div>Description:</div>
       </div>
-      <div className="attribute-values-container">
+      <div className={styles.attributeValuesContainer}>
         <div>{props.projectName}</div>
         <div>{props.parentProjectIds}</div>
         <div>{props.teamId}</div>
