63f685853d1be328b4bbf9d046d2793090106deb
robert-czwartosz <robert.czwartosz@tlen.pl>
2021-03-25 21:32:29 +0100

    create checkboxes

--- frontend/src/components/ManageUsers/ManageUsers.tsx
+++ frontend/src/components/ManageUsers/ManageUsers.tsx
@@ -1,14 +1,92 @@
-import React from 'react';
+import React, { ReactEventHandler, useEffect } from 'react';
+import { FormControlLabel, Checkbox } from '@material-ui/core';
 import styles from './ManageUsers.module.css';
 
-export interface ManageUsersProps {
+interface CheckboxProps {
   name: string;
+  checked: boolean;
+  onChange: ReactEventHandler;
+}
+
+const PrimaryCheckBox: React.FC<CheckboxProps> = ({ name, checked, onChange }) => (
+  <FormControlLabel
+    className={styles.checkbox}
+    control={
+      <Checkbox
+        name={name}
+        checked={checked}
+        onChange={onChange}
+        color="primary"
+      />
+    }
+    label={name}
+  />
+);
+
+export interface ManageUsersProps {
+  
 }
 
 const ManageUsers: React.FC< ManageUsersProps > = props => {
+  const [state, setState] = React.useState({
+    active: false,
+    archived: false,
+    resigned: false,
+    candidate: false,
+    participant: false,
+    mentor: false,
+    admin: false,
+  });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
   return (
-    <div>
-      {props.name}
+    <div className={styles.container}>
+      <h3>Sorting options</h3>
+      <div className={styles.checkboxContainer}>
+        <span>
+          <PrimaryCheckBox 
+            name='active'
+            checked={state.active}
+            onChange={handleChange}
+          />
+          <PrimaryCheckBox 
+            name='archived'
+            checked={state.archived}
+            onChange={handleChange}
+          />
+          <PrimaryCheckBox 
+            name='resigned'
+            checked={state.resigned}
+            onChange={handleChange}
+          />
+        </span>
+        <span>
+          <PrimaryCheckBox 
+            name='candidate'
+            checked={state.candidate}
+            onChange={handleChange}
+          />
+          <PrimaryCheckBox 
+            name='participant'
+            checked={state.participant}
+            onChange={handleChange}
+          />
+          <PrimaryCheckBox 
+            name='mentor'
+            checked={state.mentor}
+            onChange={handleChange}
+          />
+          <PrimaryCheckBox 
+            name='admin'
+            checked={state.admin}
+            onChange={handleChange}
+          />
+        </span>
+      </div>
     </div>
   );
 };
