1e24f587cd19ee41d2f623fbea716910590e2df4
robert-czwartosz <robert.czwartosz@tlen.pl>
2021-03-25 08:27:35 +0100

    add fixes

--- frontend/src/components/ReusableTable/ReusableTable.tsx
+++ frontend/src/components/ReusableTable/ReusableTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, MouseEvent } from 'react';
+import React, { useEffect, MouseEvent, useState } from 'react';
 import { DataGrid, GridRowParams } from '@material-ui/data-grid';
 import styles from './ReusableTable.module.css';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
@@ -34,19 +34,20 @@ const ReusableTable: React.FC< ReusableTableProps > = ({
   
   const tables = useAppSelector(selectTables);
   const dispatch = useAppDispatch();
+  const [tableName] = useState(name);
   useEffect(() => {
-    dispatch(initTable({ name }));
-    dispatch(fetchData(name, getData));
+    dispatch(initTable({ name: tableName }));
+    dispatch(fetchData(tableName, getData));
   }, []);
   return (
     <>
       { 
-        ( !tables[name] || tables[name].loading !== 'idle' ) ? (<p>Loading...</p>) :
+        ( !tables[tableName] || tables[tableName].loading !== 'idle' ) ? (<p>Loading...</p>) :
         (
-          <div className={styles.container} aria-label={'Table - ' + name}>
+          <div className={styles.container} aria-label={'Table - ' + tableName}>
             {name}
             <DataGrid
-              rows={tables[name].rows}
+              rows={tables[tableName].rows}
               columns={columns}
               pageSize={5}
               autoHeight
