d735b813ea3faef6287ea16c59a1bc3605364e12
Konrad <kj.reczek@gmail.com>
2021-03-27 13:46:22 +0100

    add method to BaseService

--- frontend/src/app/baseService.ts
+++ frontend/src/app/baseService.ts
@@ -1,7 +1,19 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL + '/api/';
+axios.defaults.headers.common = {'x-auth-token': localStorage.getItem('token')}
 
 export default class BaseService {
-    post = (path: string, ...rest: any[]) => {
-        return axios.post(process.env.REACT_APP_API_URL + '/api/' + path, ...rest)
+    post = (path: string, data: any, config?: AxiosRequestConfig) => {
+        return axios.post(path, data, config)
+    }
+    get = (path: string, config?: AxiosRequestConfig) => {
+        return axios.get(path, config)
+    }
+    delete = (path: string, config?: AxiosRequestConfig) => {
+        return axios.delete(path, config)
+    }
+    patch = (path: string, data: any, config?: AxiosRequestConfig) => {
+        return axios.patch(path, data, config)
     }
 }
