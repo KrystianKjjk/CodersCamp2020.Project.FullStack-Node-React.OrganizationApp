2ee4d6f8f018a02d2abbe24fe66396865ca682aa
Konrad <kj.reczek@gmail.com>
2021-03-27 13:47:14 +0100

    Working on logIn logic

--- frontend/src/components/LogIn/LogIn.tsx
+++ frontend/src/components/LogIn/LogIn.tsx
@@ -1,52 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 import StyledTextField from '../StyledTextField'
+import useStyles from './LogIn.style';
+import BaseService from '../../app/baseService';
 
 export interface LogInProps {
 
 };
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-
-}));
-
-const GreenCheckbox = withStyles({
-  root: {
-    color: 'white',
-    '&$checked': {
-      color: 'white',
-    },
-  },
-  checked: {},
-})((props: any) => <Checkbox color="default" {...props} />);
-
 export default function SignIn() {
   const classes = useStyles();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [formError, setFormError] = useState('');
+
+  const handleSignInClick = () => {
+    const service = new BaseService;
+    service.post('login', { email, password})
+      .then(response => {
+        setFormError('');
+        const token: any = response?.headers?.['x-auth-token'];
+        localStorage.setItem('token', token);
+      })
+      .catch(error => setFormError(error?.response?.data?.message));
+  };
+
+  
 
   return (
     <Container component="main" maxWidth="xs">
@@ -58,10 +46,11 @@ export default function SignIn() {
         <form className={classes.form} noValidate>
           <StyledTextField
             margin="normal"
-            id="email"
             label="Email Address"
             name="email"
             autoComplete="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
             autoFocus
           />
           <StyledTextField
@@ -69,19 +58,18 @@ export default function SignIn() {
             name="password"
             label="Password"
             type="password"
-            id="password"
             autoComplete="current-password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
           />
-          <FormControlLabel
-            control={<GreenCheckbox value="remember" color="primary" />}
-            label="Remember me"
-          />
+          {formError && <FormHelperText error>{'Error: ' + formError}</FormHelperText>}
           <Button
-            type="submit"
+            type="button"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
+            onClick={handleSignInClick}
           >
             Sign In
           </Button>
