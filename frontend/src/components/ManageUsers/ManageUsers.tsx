92da448dc0451ac6cf3b3572b1c27d7edcd2d6aa
robert-czwartosz <robert.czwartosz@tlen.pl>
2021-03-27 10:07:39 +0100

    craete header with search field

--- frontend/src/components/ManageUsers/ManageUsers.tsx
+++ frontend/src/components/ManageUsers/ManageUsers.tsx
@@ -3,6 +3,7 @@ import { FormControlLabel, Checkbox } from '@material-ui/core';
 import styles from './ManageUsers.module.css';
 import AddButton from '../AddButton';
 import SelectSortBy from '../SelectSortBy';
+import SearchInput from '../SearchInput';
 
 interface CheckboxProps {
   name: string;
@@ -38,7 +39,8 @@ const ManageUsers: React.FC< ManageUsersProps > = props => {
     participant: false,
     mentor: false,
     admin: false,
-    sortBy: ''
+    sortBy: '',
+    search: '',
   });
   const changeFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
     setState({ ...state, [event.target.name]: event.target.checked });
@@ -46,60 +48,69 @@ const ManageUsers: React.FC< ManageUsersProps > = props => {
   const changeSortBy = (value: string) => {
     setState({ ...state, sortBy: value });
   };
+  const changeSearch = (value: string) => {
+    setState({ ...state, search: value})
+  }
   const sortByOptions = ['Name', 'Surname', 'Type', 'Status'];
   useEffect(() => {
     console.log(state);
   }, [state]);
   return (
-    <div className={styles.manageContainer}>
-      <h2 className={styles.mainHeader}>Manage Users</h2>
-      <span className={styles.addButton}>
-        <AddButton text='Add'/>
-      </span>
-      <span className={styles.selectSortBy}>
-        <SelectSortBy onChange={changeSortBy} initialValue='' options={sortByOptions}/>
-      </span>
-      <h3 className={styles.checkboxesHeader}>Sorting options</h3>
-      <div className={styles.checkboxContainer}>
-        <span>
-          <PrimaryCheckBox 
-            name='active'
-            checked={state.active}
-            onChange={changeFilter}
-          />
-          <PrimaryCheckBox 
-            name='archived'
-            checked={state.archived}
-            onChange={changeFilter}
-          />
-          <PrimaryCheckBox 
-            name='resigned'
-            checked={state.resigned}
-            onChange={changeFilter}
-          />
+    <div className={styles.container}>
+      <div className={styles.mainHeader}>
+        <h2>Users</h2>
+        <SearchInput onSubmit={changeSearch} placeholder='User last name or ID' />
+      </div>
+      <div className={styles.manageContainer}>
+        <h2 className={styles.manageHeader}>Manage Users</h2>
+        <span className={styles.addButton}>
+          <AddButton text='Add'/>
         </span>
-        <span>
-          <PrimaryCheckBox 
-            name='candidate'
-            checked={state.candidate}
-            onChange={changeFilter}
-          />
-          <PrimaryCheckBox 
-            name='participant'
-            checked={state.participant}
-            onChange={changeFilter}
-          />
-          <PrimaryCheckBox 
-            name='mentor'
-            checked={state.mentor}
-            onChange={changeFilter}
-          />
-          <PrimaryCheckBox 
-            name='admin'
-            checked={state.admin}
-            onChange={changeFilter}
-          />
+        <span className={styles.selectSortBy}>
+          <SelectSortBy onChange={changeSortBy} initialValue='' options={sortByOptions}/>
         </span>
+        <h3 className={styles.checkboxesHeader}>Sorting options</h3>
+        <div className={styles.checkboxContainer}>
+          <span>
+            <PrimaryCheckBox 
+              name='active'
+              checked={state.active}
+              onChange={changeFilter}
+            />
+            <PrimaryCheckBox 
+              name='archived'
+              checked={state.archived}
+              onChange={changeFilter}
+            />
+            <PrimaryCheckBox 
+              name='resigned'
+              checked={state.resigned}
+              onChange={changeFilter}
+            />
+          </span>
+          <span>
+            <PrimaryCheckBox 
+              name='candidate'
+              checked={state.candidate}
+              onChange={changeFilter}
+            />
+            <PrimaryCheckBox 
+              name='participant'
+              checked={state.participant}
+              onChange={changeFilter}
+            />
+            <PrimaryCheckBox 
+              name='mentor'
+              checked={state.mentor}
+              onChange={changeFilter}
+            />
+            <PrimaryCheckBox 
+              name='admin'
+              checked={state.admin}
+              onChange={changeFilter}
+            />
+          </span>
+        </div>
       </div>
     </div>
   );
