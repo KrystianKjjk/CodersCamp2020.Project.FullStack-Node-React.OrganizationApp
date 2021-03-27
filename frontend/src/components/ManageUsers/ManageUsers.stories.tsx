6046f14cea379aedf6ff0e6ca9521c810a877f89
robert-czwartosz <robert.czwartosz@tlen.pl>
2021-03-27 20:40:09 +0100

    update story

--- frontend/src/components/ManageUsers/ManageUsers.stories.tsx
+++ frontend/src/components/ManageUsers/ManageUsers.stories.tsx
@@ -16,17 +16,12 @@ const Template: Story<ManageUsersProps> = (args) => (
     <ManageUsers {...args} />
   </Provider>
 );
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDRjNjQyZTUzNDdhZDE5ZDRmOWE0MzciLCJ0eXBlIjozLCJpYXQiOjE2MTY4NDQxNzksImV4cCI6MTYxNjg0NTM3OX0.FLhyRwah2nmU-DycG3oPGmTOo4hM1GBz893BKsT-vt8';
+
 const getUsers = () => {
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDRjNjQyZTUzNDdhZDE5ZDRmOWE0MzciLCJ0eXBlIjozLCJpYXQiOjE2MTY4NzIzNTcsImV4cCI6MTYxNjk1ODc1N30.RX1EzN6tHmdMFhDWtm6TmQPFzML6min3e_11RH3B6GA';
   return getUsersApi(token);
 }
 export const SampleManageUsers = Template.bind({});
 SampleManageUsers.args = {
   getUsers: getUsers,
 };
-//Promise.resolve([
-    // {id: 1, name: 'Name1', surname: 'Surname1', type: 'Admin', status: 'Active'},
-    // {id: 2, name: 'Name2', surname: 'Surname2', type: 'Admin', status: 'Active'},
-    // {id: 3, name: 'Name3', surname: 'Surname3', type: 'Admin', status: 'Active'},
-    // {id: 4, name: 'Name4', surname: 'Surname4', type: 'Admin', status: 'Active'},
-    //]);
