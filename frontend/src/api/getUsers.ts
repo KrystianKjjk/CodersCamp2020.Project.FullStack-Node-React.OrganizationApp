4438f92472362a889f62e080e3c2d8cedd352e58
robert-czwartosz <robert.czwartosz@tlen.pl>
2021-03-27 20:38:56 +0100

    transform uset type and status

--- frontend/src/api/getUsers.ts
+++ frontend/src/api/getUsers.ts
@@ -1,11 +1,47 @@
+import api from './api';
+
+
+enum UserType {
+    Candidate,
+    Participant,
+    Mentor,
+    Admin,
+};
+enum UserStatus {
+    Active,
+    Resigned,
+    Archived,
+};
+interface User {
+    _id: string;
+    name: string;
+    surname: string;
+    status: UserStatus;
+    type: UserType;
+}
+const userStatusDict = {
+    [UserStatus.Active]: 'Active',
+    [UserStatus.Archived]: 'Archived',
+    [UserStatus.Resigned]: 'Resigned',
+}
+const userTypeDict = {
+    [UserType.Candidate]: 'Candidate',
+    [UserType.Participant]: 'Participant',
+    [UserType.Mentor]: 'Mentor',
+    [UserType.Admin]: 'Admin',
+}
+
 export default async function getUsers(authToken: string): Promise<any[]> {
-    const response = await fetch('https://coders-camp-organization-app.herokuapp.com/api/users', {
-        method: 'GET',
+    const config = {
         headers: {
             'x-auth-token': authToken,
         }
-    });
-    const json = await response.json();
-    if( !(json.ok) ) throw Error(json.message);
-    return json as any[];
+    };
+    const response = await api.get<User[]>('/users', config);
+    return response.data.map( user => ({
+        ...user,
+        id: user._id,
+        type: userTypeDict[user.type],
+        status: userStatusDict[user.status],
+    }) );
 }
