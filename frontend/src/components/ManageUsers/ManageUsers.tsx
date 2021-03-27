6822c1763427969f9eb4f3ee5d6c27750f3e72a9
robert-czwartosz <robert.czwartosz@tlen.pl>
2021-03-27 09:45:55 +0100

    implement manage users window

--- frontend/src/components/ManageUsers/ManageUsers.tsx
+++ frontend/src/components/ManageUsers/ManageUsers.tsx
@@ -2,6 +2,7 @@ import React, { ReactEventHandler, useEffect } from 'react';
 import { FormControlLabel, Checkbox } from '@material-ui/core';
 import styles from './ManageUsers.module.css';
 import AddButton from '../AddButton';
+import SelectSortBy from '../SelectSortBy';
 
 interface CheckboxProps {
   name: string;
@@ -37,56 +38,66 @@ const ManageUsers: React.FC< ManageUsersProps > = props => {
     participant: false,
     mentor: false,
     admin: false,
+    sortBy: ''
   });
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const changeFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
+  const changeSortBy = (value: string) => {
+    setState({ ...state, sortBy: value });
+  };
+  const sortByOptions = ['Name', 'Surname', 'Type', 'Status'];
   useEffect(() => {
     console.log(state);
   }, [state]);
   return (
-    <div className={styles.container}>
+    <div className={styles.manageContainer}>
       <h2 className={styles.mainHeader}>Manage Users</h2>
-      <span className={styles.addButton}><AddButton text='Add'/></span>
+      <span className={styles.addButton}>
+        <AddButton text='Add'/>
+      </span>
+      <span className={styles.selectSortBy}>
+        <SelectSortBy onChange={changeSortBy} initialValue='' options={sortByOptions}/>
+      </span>
       <h3 className={styles.checkboxesHeader}>Sorting options</h3>
       <div className={styles.checkboxContainer}>
         <span>
           <PrimaryCheckBox 
             name='active'
             checked={state.active}
-            onChange={handleChange}
+            onChange={changeFilter}
           />
           <PrimaryCheckBox 
             name='archived'
             checked={state.archived}
-            onChange={handleChange}
+            onChange={changeFilter}
           />
           <PrimaryCheckBox 
             name='resigned'
             checked={state.resigned}
-            onChange={handleChange}
+            onChange={changeFilter}
           />
         </span>
         <span>
           <PrimaryCheckBox 
             name='candidate'
             checked={state.candidate}
-            onChange={handleChange}
+            onChange={changeFilter}
           />
           <PrimaryCheckBox 
             name='participant'
             checked={state.participant}
-            onChange={handleChange}
+            onChange={changeFilter}
           />
           <PrimaryCheckBox 
             name='mentor'
             checked={state.mentor}
-            onChange={handleChange}
+            onChange={changeFilter}
           />
           <PrimaryCheckBox 
             name='admin'
             checked={state.admin}
-            onChange={handleChange}
+            onChange={changeFilter}
           />
         </span>
       </div>
