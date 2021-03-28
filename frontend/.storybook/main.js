dc3041019cf48df72b0e141434d32ef286087063
Justyna <justyna.smoliga@gmail.com>
2021-03-28 15:03:19 +0200

    storybook main.js changes removed

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -6,7 +6,6 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/storybook-css-modules-preset"
+    "@storybook/preset-create-react-app"
   ]
 }
