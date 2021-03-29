18e4e4fb98d35f851b6e68f83c1719ecb7d4d831
FIlip <filip.szwedo@gmail.com>
2021-03-29 19:26:12 +0200

    story update

--- frontend/src/components/TeamProject/TeamProject.stories.tsx
+++ frontend/src/components/TeamProject/TeamProject.stories.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import TeamProject , { TeamProjectProps } from './TeamProject';
+import { Provider } from 'react-redux';
+import { store } from '../../app/store';
 
 export default {
   title: 'TeamProject component',
   component: TeamProject,
 } as Meta;
 
-const Template: Story<TeamProjectProps> = (args) => <TeamProject {...args} />;
+const Template: Story<TeamProjectProps> = (args) => {
+  <Provider store ={store}>
+    <TeamProject {...args} />;
+  </Provider>
+}
 
 export const TeamProjectStory = Template.bind({});
 TeamProjectStory.args = {
-  _id: "123123",
-  // teamId: "123123",
-  // parentProjectIds: "!23123",
-  // projectName: "FitNotFat",
-  // projectUrl: "fitnotfat.com",
-  // description: "Aplikacja dostarcza użytkownikowi informacje ile kalorii dziennie powinien spożywać, aby osiągnąć określony cel: utrzymać obecną wagę, schudnąć lub zwiększyć masę ciała."
+  _id: "60426bdd9eb5d008d4a956cc",
 };
