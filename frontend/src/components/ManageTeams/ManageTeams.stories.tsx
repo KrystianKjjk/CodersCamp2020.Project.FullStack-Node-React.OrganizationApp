0d380c8140bb3d5ca1f11cbbaf57a102593133fc
robert-czwartosz <robert.czwartosz@tlen.pl>
2021-03-31 18:39:39 +0200

    add story

--- frontend/src/components/ManageTeams/ManageTeams.stories.tsx
+++ frontend/src/components/ManageTeams/ManageTeams.stories.tsx
@@ -33,10 +33,10 @@ SampleFakeManageTeams.args = {
   onClickAdd: () => console.log('Clicked Add'),
 };
 
-const authToken = '';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDRjNjQyZTUzNDdhZDE5ZDRmOWE0MzciLCJ0eXBlIjozLCJpYXQiOjE2MTY4NzIzNTcsImV4cCI6MTYxNjk1ODc1N30.RX1EzN6tHmdMFhDWtm6TmQPFzML6min3e_11RH3B6GA';
 
 export const SampleManageTeams = Template.bind({});
 SampleManageTeams.args = {
-  getTeams: () => getTeams(authToken),
+  getTeams: () => getTeams(token),
   onClickAdd: () => console.log('Clicked Add'),
 };
