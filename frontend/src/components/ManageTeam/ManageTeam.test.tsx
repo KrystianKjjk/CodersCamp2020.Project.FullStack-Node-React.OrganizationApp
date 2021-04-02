340bb2943e7e7fab0251536f7402c0c3716b1727
robert-czwartosz <robert.czwartosz@tlen.pl>
2021-04-02 12:57:01 +0200

    fix tests

--- frontend/src/components/ManageTeam/ManageTeam.test.tsx
+++ frontend/src/components/ManageTeam/ManageTeam.test.tsx
@@ -6,65 +6,44 @@ import { store } from '../../app/store';
 import ManageTeam from './ManageTeam';
 import { sortData, filterData } from '../ReusableTable/ReusableTableSlice';
 
-const teamsDatabase = [
-   {id: 1, name: 'Naame', surname: 'Suurname', courseName: 'CodersCamp 1. edition'},
-   {id: 2, name: 'Naaame', surname: 'Suuuurname', courseName: 'CodersCamp 1. edition'},
-   {id: 3, name: 'Naaaame', surname: 'Suuurname', courseName: 'CodersCamp 1. edition'},
-   {id: 4, name: 'Naaaaaame', surname: 'Suuuuurname', courseName: 'CodersCamp 1. edition'},
-   {id: 5, name: 'CName', surname: 'CSurname', courseName: 'CodersCamp 1. edition'},
- ]
-const getFakeTeams = () => {
-   return Promise.resolve(teamsDatabase);
-}
+const teamsDatabase = [{
+   id: '0',
+   mentor: {
+     name: 'Naame',
+     surname: 'Suurname',
+   },
+   users: [{
+     id: '11',
+     name: 'UserName',
+     surname: 'UserSurname',
+     status: 'Active',
+     averageGrade: 11,
+   }],
+   projects: [],
+   teamAvgGrade: 11,
+   maxPoints: 111,
+ }];
+ const getFakeTeam = (id: string) => {
+   return Promise.resolve(teamsDatabase[Number(id)]);
+ }
 
 describe('ManageTeam', () => {
    it('renders without error', async () => {
-      const getTeams = jest.fn( getFakeTeams );
+      const getTeam = jest.fn( getFakeTeam );
       const onClickAdd = jest.fn();
       render(
          <Provider store={store}>
-            <ManageTeam onClickAdd={onClickAdd} getTeamMembers={getFakeTeams}/>
+            <ManageTeam teamId='0' onClickAdd={onClickAdd} getTeamInfo={getTeam}/>
          </Provider>
       );
-      expect(getTeams).toBeCalledTimes(1);
-      const addBtn = screen.getByLabelText('Add team');
+      const tableComp = await screen.findByLabelText('Table - Team');
+      expect(getTeam).toBeCalledTimes(1);
+      const addBtn = screen.getByLabelText('Add user');
       userEvent.click(addBtn);
       expect(onClickAdd).toBeCalledTimes(1);
-      const tableComp = await screen.findByLabelText('Table - Teams');
-      const table = store.getState().tables['Teams'];
-      expect(table.rows).toHaveLength(teamsDatabase.length);
-   });
-
-   it('search input works', async () => {
-      const getTeams = jest.fn( getFakeTeams );
-      const onClickAdd = jest.fn();
-      render(
-         <Provider store={store}>
-            <ManageTeam onClickAdd={onClickAdd} getTeamMembers={getFakeTeams}/>
-         </Provider>
-      );
-
-      const tableComp = await screen.findByLabelText('Table - Teams');
-      store.dispatch(filterData({
-         table: 'Teams',
-         filters: [ {column: 'surname', values: ['CSurname']} ]
-      }));
-      const table = store.getState().tables['Teams'];
-      expect(table.displayedRows).toHaveLength(1);
-   });
-
-   it('sort list works', async () => {
-      const getTeams = jest.fn( getFakeTeams );
-      const onClickAdd = jest.fn();
-      render(
-         <Provider store={store}>
-            <ManageTeam onClickAdd={onClickAdd} getTeamMembers={getFakeTeams}/>
-         </Provider>
-      );
-      const tableComp = await screen.findByLabelText('Table - Teams');
-      store.dispatch(sortData({table: 'Teams', column: 'name'}));
-      const table = store.getState().tables['Teams'];
-      expect(table.displayedRows[0].name).toBe('CName');
+      
+      const table = store.getState().tables['Team'];
+      expect(table.rows).toHaveLength(teamsDatabase[0].users.length);
    });
 
 });
