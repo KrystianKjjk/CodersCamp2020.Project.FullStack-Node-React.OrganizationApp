acfc5c32b8662ce670bc4d4a5b1a1429f6b114f6
Konrad <kj.reczek@gmail.com>
2021-04-08 16:21:32 +0200

    add edit mode to section view

--- frontend/src/components/SectionView/SectionView.tsx
+++ frontend/src/components/SectionView/SectionView.tsx
@@ -11,18 +11,30 @@ import { Container, CssBaseline, Paper, TextField } from '@material-ui/core';
 import BaseService from '../../app/baseService';
 import { useParams } from 'react-router-dom';
 import SectionService from '../../api/Section.service';
+import UButton from "../UButton";
+import StyledTextField from '../StyledTextField';
+import DateFnsUtils from '@date-io/date-fns'; 
+import 'date-fns'
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+  DatePicker,
+} from "@material-ui/pickers";
 
 export interface SectionViewProps {
+  editMode?: boolean;
 }
 
-const SectionView: React.FC< SectionViewProps > = () => {
+const SectionView: React.FC< SectionViewProps > = (editMode) => {
   const sectionService = new SectionService();
   const [sectionName, setSectionName] = useState("");
   const [courseName, setCourseName] = useState("");
   const [description, setDescription] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [referenceProject, setReferenceProject] = useState("")
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+  const [endDate, setEndDate] = useState<Date | null>(new Date());
+  const [referenceProject, setReferenceProject] = useState("");
+  const [isInEditMode, setIsInEditMode] = useState(false);
   const { id } = useParams();
 
   const deleteSection = () => {
@@ -30,6 +42,10 @@ const SectionView: React.FC< SectionViewProps > = () => {
   }
 
   const editSection = () => {
+    console.log('test');
+  }
+
+  const saveSection = () => {
     console.log('test');
   }
 
@@ -41,10 +57,10 @@ const SectionView: React.FC< SectionViewProps > = () => {
     setDescription(e.target.value);
   };
 
-  const displayFormattedDate = (date?: Date) => {
+  const displayFormattedDate = (date: Date | null) => {
     if (!date) return '';
 
-    return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+    return `${date.toLocaleDateString()}`;
   }
 
   const getOneSectionData = async () => {
@@ -53,14 +69,65 @@ const SectionView: React.FC< SectionViewProps > = () => {
     setSectionName(data.name);
     setCourseName(data.courseName);
     setDescription(data.description || '');
-    setStartDate(displayFormattedDate(data.startDate));
-    setEndDate(displayFormattedDate(data.endDate));
+    setStartDate(data.startDate || null);
+    setEndDate(data.endDate || null);
     setReferenceProject(data.referenceProjectName || '');
   };
   
   useEffect(() => {
       getOneSectionData();
   }, [])
+
+  const renderButtonEditSave = () => {
+    if (isInEditMode) {
+      return (
+        <span onClick={saveSection} className={styles.editButton} aria-label='Save section'>
+          <UButton text='SAVE' color='primary' onClick={() => setIsInEditMode(true)}/>
+          </span>
+      )
+    } else {
+      return (
+        <span onClick={editSection} className={styles.editButton} aria-label='Edit section'>
+          <UButton text='EDIT' color='primary' onClick={() => setIsInEditMode(true)}/>
+          </span>
+      )
+    }
+  }
+
+  const renderSectionName = () => {
+    if (isInEditMode) {
+      return (
+        <div className={styles.textFieldSectionName}>
+        <StyledTextField
+          value={sectionName}
+          onChange={e => setSectionName(e.target.value)}
+          onBlur={() => console.log({sectionName})}
+        /></div>
+      )
+    } else {
+      return (
+        <span className={styles.section_title}> {sectionName} </span>
+      )
+    }
+  }
+
+  const renderDescription = () => {
+    if (isInEditMode) {
+      return (
+        <div className={styles.textFieldDescription}>
+        <StyledTextField
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+          multiline={true}
+          onBlur={() => console.log({description})}
+        /></div>
+      )
+    } else {
+      return (
+        <span> Description: {description} </span>
+      )
+    }
+  }
 
   return (
     <Container className={styles.manageSections} aria-label='Manage Section'>
@@ -71,30 +138,55 @@ const SectionView: React.FC< SectionViewProps > = () => {
       <Paper className={styles.container}>
         <div className={styles.manageContainer}>
           <h2 className={styles.manageHeader}>Manage Section</h2>
+          <div className={styles.buttonContainer}>
           <span onClick={deleteSection} className={styles.deleteButton} aria-label='Delete section'>
-            <DeleteButton text='Delete'/>
+            <UButton text='DELETE' color='secondary' onClick={console.log("string")}/>
           </span>
-          <span onClick={editSection} className={styles.editButton} aria-label='Edit section'>
-            <AddButton text='Edit'/>
-          </span>
+          {renderButtonEditSave()}
+          </div>
         </div>
         <div className={styles.manageDescription}>
-          <span className={styles.section_title}> {sectionName} </span>
-          <span> {courseName}</span>
-          <span> description: {description} </span>
+          {renderSectionName()}
+          <span className={styles.course_title}> {courseName}</span>
+          {renderDescription()}
         </div>
           <div className={styles.manageSectionInfo}> 
             <div className={styles.manageSectionMaterial}> 
-              <h3>Material:</h3>
-              <span>One</span>
+              <h3>Materials:</h3>
+              <span>Introduction</span>
             </div>
             <div className={styles.manageSectionDates}> 
               <h3>Dates:</h3>
-              <p>{startDate}</p>
-              <p>{endDate}</p>
+              <div className={styles.textFieldDateContainerOnEdit}>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <DatePicker
+                    disableToolbar={!isInEditMode}
+                    disabled={!isInEditMode}
+                    variant="inline"
+                    format="dd/MM/yyyy"
+                    id="date-picker-inline"
+                    label="Start date"
+                    inputVariant="outlined"
+                    value={startDate}
+                    onChange={e => setStartDate(e)}
+                  />
+                  <DatePicker
+                    disableToolbar={!isInEditMode}
+                    disabled={!isInEditMode}
+                    variant="inline"
+                    format="dd/MM/yyyy"
+                    id="date-picker-inline"
+                    label="End date"
+                    inputVariant="outlined"
+                    autoOk
+                    value={endDate}
+                    onChange={e => setEndDate(e)}
+                  />
+                  </MuiPickersUtilsProvider>
+              </div>
             </div>
             <div className={styles.manageSectionRefPro}> 
-              <h3>Referenece project:</h3>
+              <h3>Reference project:</h3>
               <p>{referenceProject}</p>
             </div>
         </div>
