c0d15153706c7b4aa463e250d5cd2304175a6b6b
FIlip <filip.szwedo@gmail.com>
2021-04-09 16:12:51 +0200

    logout handling

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Header.module.css'
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 import {removeUserFromLocalStorage} from '../../app/utils'
+import { useHistory } from 'react-router-dom';
 
 const Header: React.FC= () => {
 
@@ -9,10 +10,21 @@ const Header: React.FC= () => {
     removeUserFromLocalStorage();
   }
 
+  const history = useHistory();
+  const routeChange = () => { 
+    let path = `/`; 
+    history.push(path);
+}
+
+
   return (
     <div className={styles.header}>
         <div className={styles.logo}><span>.</span>Coders<span>Camp</span></div>
-        <div className={styles.logout} onClick={() => handleLogOut()}><PowerSettingsNewIcon style={{color:"rgba(255, 255, 255, 0.6)"}}></PowerSettingsNewIcon><span> Log out</span></div>
+        <div className={styles.logout} onClick={() => {
+          handleLogOut();
+          routeChange();
+        }
+        }><PowerSettingsNewIcon style={{color:"rgba(255, 255, 255, 0.6)"}}></PowerSettingsNewIcon><span> Log out</span></div>
     </div>
    );
 };
