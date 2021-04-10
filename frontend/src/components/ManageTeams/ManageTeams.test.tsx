f23742282567ecf378843883eaecb94e1fcff70e
robert-czwartosz <robert.czwartosz@tlen.pl>
2021-04-10 09:20:50 +0200

    mock createTeam

--- frontend/src/components/ManageTeams/ManageTeams.test.tsx
+++ frontend/src/components/ManageTeams/ManageTeams.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, wait } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { store } from '../../app/store';
@@ -14,30 +14,37 @@ const teamsDatabase = [
    {id: 4, name: 'Naaaaaame', surname: 'Suuuuurname', courseName: 'CodersCamp 1. edition'},
    {id: 5, name: 'CName', surname: 'CSurname', courseName: 'CodersCamp 1. edition'},
 ];
+const teamsCount = teamsDatabase.length;
 
 jest.mock('../../api/Team.service.ts', () => jest.fn());
       
 describe('ManageTeams', () => {
    it('renders without error, filters and sorts data', async () => {
       const getTeamsMock = jest.fn( () => Promise.resolve(teamsDatabase) );
+      const createTeamMock = jest.fn( async () => {
+         teamsDatabase.push({id: 7, name: '---', surname: '---', courseName: 'Course from local storage'})
+      } );
       const TeamServiceMock = {
          getTeams: getTeamsMock,
+         createTeam: createTeamMock,
       };
       // @ts-ignore
       TeamService.mockImplementation(() => TeamServiceMock);
-      const onClickAdd = jest.fn();
+      
       render(
          <Provider store={store}>
-            <ManageTeams onClickAdd={onClickAdd} />
+            <ManageTeams />
          </Provider>
       );
+      
+      await screen.findByLabelText('Table - Teams');
       expect(getTeamsMock).toBeCalledTimes(1);
+      expect(store.getState().tables['Teams'].rows).toHaveLength(teamsCount);
       const addBtn = screen.getByLabelText('Add team');
       userEvent.click(addBtn);
-      expect(onClickAdd).toBeCalledTimes(1);
-      const tableComp = await screen.findByLabelText('Table - Teams');
-      const table = store.getState().tables['Teams'];
-      expect(table.rows).toHaveLength(teamsDatabase.length);
+      expect(createTeamMock).toBeCalledTimes(1);
+      expect(getTeamsMock).toBeCalledTimes(1);
+      wait(() => expect(store.getState().tables['Teams'].rows).toHaveLength(teamsCount + 1));
 
       store.dispatch(filterData({
          table: 'Teams',
