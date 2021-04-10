6ac477929c78976d45d32e1e4d40b3798ac49afa
robert-czwartosz <robert.czwartosz@tlen.pl>
2021-04-10 12:22:15 +0200

    remove getUsers prop

--- frontend/src/components/ManageUsers/ManageUsers.stories.tsx
+++ frontend/src/components/ManageUsers/ManageUsers.stories.tsx
@@ -23,24 +23,7 @@ const Template: Story<ManageUsersProps> = (args) => (
 
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDRjNjQyZTUzNDdhZDE5ZDRmOWE0MzciLCJ0eXBlIjozLCJpYXQiOjE2MTY4NzIzNTcsImV4cCI6MTYxNjk1ODc1N30.RX1EzN6tHmdMFhDWtm6TmQPFzML6min3e_11RH3B6GA';
 localStorage.setItem('token', token);
-const api = new UserService();
+localStorage.setItem('courseId', '604bd56eef20be4368273700');
 
 export const SampleManageUsers = Template.bind({});
-SampleManageUsers.args = {
-  getUsers: api.getUsers,
-};
-
-const getFakeUsers = () => {
-  return Promise.resolve([
-    {id: 1, name: 'Naame', surname: 'Suurname', type: 'Mentor', status: 'Active'},
-    {id: 2, name: 'Naaame', surname: 'Suuuurname', type: 'Participant', status: 'Archived'},
-    {id: 3, name: 'Naaaame', surname: 'Suuurname', type: 'Participant', status: 'Resigned'},
-    {id: 4, name: 'Naaaaaame', surname: 'Suuuuurname', type: 'Admin', status: 'Active'},
-    {id: 5, name: 'CName', surname: 'CSurname', type: 'Candidate', status: 'Active'},
-  ])
-}
-
-export const SampleManageFakeUsers = Template.bind({});
-SampleManageFakeUsers.args = {
-  getUsers: getFakeUsers,
-};
+SampleManageUsers.args = { };
