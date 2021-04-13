7d12a2b67d83eec984b899ec193bd100a49aa4d0
FIlip <filip.szwedo@gmail.com>
2021-04-13 22:46:21 +0200

    password reset added

--- frontend/src/components/MyProfile/MyProfile.tsx
+++ frontend/src/components/MyProfile/MyProfile.tsx
@@ -2,8 +2,17 @@ import React, {useState, useEffect} from 'react';
 import styles from './MyProfile.module.css';
 import UserService from "../../api/users.service";
 import BaseService from "../../app/baseService";
-import {CircularProgress} from "@material-ui/core";
+import {
+  CircularProgress, 
+  Button, 
+  Grid,
+  Snackbar,  
+  FormHelperText
+} from "@material-ui/core";
 import {IUser} from "../../models/User.model";
+import StyledTextField from '../StyledTextField'
+import { Error } from 'mongoose';
+import MuiAlert, { AlertProps }  from '@material-ui/lab/Alert';
 
 export interface MyProfileProps {
 }
@@ -14,6 +23,13 @@ const MyProfile = () => {
   const [user, setUser] = useState<IUser | undefined>(undefined);
   const [error, setError] = useState();
   const [isLoaded, setIsLoaded] = useState(false);
+  const [isPasswordChange, setIsPasswordChange] = useState(false);
+
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [passwordChanged, setPasswordChanged] = useState(false);
+  
+  const [openError, setOpenError] = useState(false);
 
   useEffect(() => {
     getUser();
@@ -44,6 +60,102 @@ const MyProfile = () => {
     })
 }
 
+function Alert(props: AlertProps) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const handleCloseError = (event?: React.SyntheticEvent, reason?: string) => {
+  if (reason === 'clickaway') {
+    return;
+  }
+  setOpenError(false);
+};
+
+const handleSubmit = async () => {
+  const service = new BaseService();
+  const id = localStorage.getItem('id');
+  try {
+    await service.post('users/changepassword', { id, oldPassword, newPassword })
+    setPasswordChanged(true);
+  }
+  catch (error) {
+    console.log(error)
+    setOpenError(true);
+  };
+};
+
+  const PasswordChangeModal = () => {
+    if (isPasswordChange && !passwordChanged) {
+      return (
+        <div className={styles.modal}>
+          <div className={styles.modalContent}>
+            <h4>Please provide old and new password to proceed:</h4>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <StyledTextField
+                  value={oldPassword}
+                  onChange={e => setOldPassword(e.target.value)}
+                  name="password"
+                  label="Old Password"
+                  type="password"
+                  data-testid="r-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <StyledTextField
+                  value={newPassword}
+                  onChange={e => setNewPassword(e.target.value)}
+                  name="passwordconfirm"
+                  label="New Password"
+                  type="password"
+                  data-testid="r-cpassword"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              id={styles.buttonEditPassword}
+              data-testid="rp-button"
+              onClick={handleSubmit}
+            >
+              Send
+          </Button>
+
+            <div>
+              <Button id={styles.buttonAbort} onClick={() => setIsPasswordChange(!isPasswordChange)}>Cancel</Button>
+            </div>
+          </div>
+
+          <Snackbar open={openError} autoHideDuration={6000} onClose={handleCloseError} data-testid='li-snack'>
+            <Alert onClose={handleCloseError} severity="error">
+              <FormHelperText >Something went wrong :( Please try again</FormHelperText>
+            </Alert>
+          </Snackbar>
+        </div>)
+    }
+    else if (isPasswordChange && passwordChanged) {
+      return (
+        <div className={styles.modal}>
+           <div className={styles.modalContent}>
+               <h3>Your password had been changed :)</h3>
+               <div>
+                <Button id={styles.buttonConfirm} onClick={() => {
+                  setIsPasswordChange(!isPasswordChange);
+                  setPasswordChanged(false);
+                }                
+                }>Cool, thank you!</Button>
+            </div>
+           </div>
+        </div>       
+      )
+    }
+    else return null
+  }
+
+
 if (error) {
   return <div className={styles.error}>Something went wrong :(</div>;
 } else if (!isLoaded || !user) {
@@ -62,6 +174,8 @@ if (error) {
             <div>Permission Level:</div>
             <div>Status:</div>
             <div>Email:</div>
+            <br/><br/>
+            <div>Change password</div>
           </div>
           <div className={styles.attributeValuesContainer}>
             <div>{user.name}</div>
@@ -69,11 +183,18 @@ if (error) {
             <div>
                 <PermissionLabel type={user.type}/>
             </div>
-            <div><StatusLabel status={user.status}/></div>
+            <div>
+              <StatusLabel status={user.status}/>
+              </div>
             <div>{user.email}</div>
+            <br/><br/>
+            <div>
+              <Button id={styles.buttonEdit} onClick= {() => {setIsPasswordChange(!isPasswordChange)}}>Change</Button>
+            </div>
           </div>
         </div>
-      </div>
+        <PasswordChangeModal/>
+      </div>      
   )
 }
 };
