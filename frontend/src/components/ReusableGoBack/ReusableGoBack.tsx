cfde3403fe456d2c3d4e1c85cd01fca6e49a63a8
Konrad <kj.reczek@gmail.com>
2021-04-29 15:04:32 +0200

    change styles in reusablego back component

--- frontend/src/components/ReusableGoBack/ReusableGoBack.tsx
+++ frontend/src/components/ReusableGoBack/ReusableGoBack.tsx
@@ -1,14 +1,24 @@
+import { Breadcrumbs, Link, Typography } from '@material-ui/core';
 import React from 'react';
 import styles from './ReusableGoBack.module.css';
+import ArrowBackRoundedIcon from '@material-ui/icons/ArrowBackRounded';
 
 export interface ReusableGoBackProps {
-  name: string;
+  pageName: string;
+  pageLink: string;
+  elementName: string;
 }
 
 const ReusableGoBack: React.FC< ReusableGoBackProps > = props => {
   return (
-    <div>
-      {props.name}
+    <div className={styles.container_goback}>
+      <Breadcrumbs aria-label="breadcrumb" className={styles.breadcrumbs}>
+          <Link href={props.pageLink} color="textPrimary" className={styles.breadcrumbs_link}> 
+            <ArrowBackRoundedIcon className={styles.icon_back} />
+            {props.pageName}
+          </Link>
+        <Typography>{props.elementName}</Typography>
+      </Breadcrumbs>
     </div>
   );
 };
