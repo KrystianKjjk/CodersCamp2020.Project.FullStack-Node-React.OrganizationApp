fe3f42ed5e9df192b9e3eead3fa13994d041cbe2
robert-czwartosz <robert.czwartosz@tlen.pl>
2021-05-01 21:27:00 +0200

    add fixes

--- frontend/src/hooks/useMultipleSelect/useMultipleSelect.tsx
+++ frontend/src/hooks/useMultipleSelect/useMultipleSelect.tsx
@@ -1,52 +1,91 @@
-import React, { useState } from "react";
-import MenuItem from "@material-ui/core/MenuItem";
-import TextField from "@material-ui/core/TextField";
-import styles from "./useMultipleSelect.module.css";
-import { Container } from "@material-ui/core";
+import React, { useState } from 'react'
+import MenuItem from '@material-ui/core/MenuItem'
+import {
+  Container,
+  FormControl,
+  Input,
+  InputLabel,
+  Select,
+  Theme,
+  useTheme,
+} from '@material-ui/core'
+import styles from './useMultipleSelect.module.css'
+import { MenuProps } from '../../components/Menu/Menu'
 
-
-export interface useMultipleSelectArgs {
-  label: string;
-  options: string[];
-  labels?: {[option: string]: string;};
+const ITEM_HEIGHT = 48
+const ITEM_PADDING_TOP = 8
+const menuProps: MenuProps = {
+  getContentAnchorEl: null,
+  anchorOrigin: { vertical: "bottom", horizontal: "center" },
+  transformOrigin: { vertical: "top", horizontal: "center" },
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 200,
+    },
+  },
 }
 
-export default function useMultipleSelect(
-  { options, label, labels = {} }: useMultipleSelectArgs
-): [ JSX.Element, string[] ] {
-  const [selected, setSelected] = useState<string[]>([]);
+function getStyles(name: string, selected: string[], theme: Theme) {
+  return {
+    fontWeight:
+      selected.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  }
+}
 
-  const handleFieldChange = (
+export interface useMultipleSelectArgs {
+  label: string
+  options: string[]
+  labels?: { [option: string]: string }
+}
+
+export default function useMultipleSelect({
+  options,
+  label,
+  labels = {},
+}: useMultipleSelectArgs): [JSX.Element, string[]] {
+  const theme = useTheme()
+  const [selected, setSelected] = useState<string[]>([])
+
+  const handleChange = (
     event: React.ChangeEvent<{
-      name?: string | undefined;
-      value: unknown;
-    }>
+      name?: string | undefined
+      value: unknown
+    }>,
   ) => {
-    event.persist();
-    console.log(event.target.value)
-    setSelected(event.target.value as string[]);
-  };
+    setSelected(event.target.value as string[])
+  }
   const multipleSelectComponent = (
-    <Container className={styles.select}>
-      <TextField
-        className={styles.select}
-        select
-        name={label}
-        id={label}
-        variant="outlined"
-        label={label}
-        value={selected}
-        SelectProps={{
-          multiple: true,
-          value: selected,
-          onChange: handleFieldChange
-        }}
-      >
-        {options.map(option => (
-          <MenuItem className={styles.select} key={option} value={option}> {labels[option] ?? option} </MenuItem>
-        ))}
-      </TextField>
+    <Container>
+      <FormControl className={styles.select}>
+        <InputLabel>{label}</InputLabel>
+        <Select
+          multiple
+          value={selected}
+          onChange={handleChange}
+          input={<Input />}
+          renderValue={(selected) => {
+            const items = (selected as string[]).length
+            return (
+              <div>{`Selected ${items} item${items !== 1 ? 's' : ''}`}</div>
+            )
+          }}
+          MenuProps={menuProps}
+        >
+          {options.map((option) => (
+            <MenuItem
+              key={option}
+              value={option}
+              style={getStyles(option, selected, theme)}
+            >
+              {labels[option] ?? option}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
     </Container>
-  );
-  return [multipleSelectComponent, selected];
+  )
+  return [multipleSelectComponent, selected]
 }
