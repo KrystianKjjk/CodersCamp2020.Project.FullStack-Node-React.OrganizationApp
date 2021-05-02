3f57f73d6bda1e2191bd945bc60e8e31c3d4f4a9
ireneusz <ireneusz.petryszak@gmail.com>
2021-05-02 19:56:02 +0200

    fixes

--- frontend/src/app/baseService.ts
+++ frontend/src/app/baseService.ts
@@ -23,7 +23,7 @@ axios.interceptors.response.use( response => response,
                         return _redirect();
                     }
                 default:
-                    return;
+                    return Promise.reject(error);
             }
         } else {
             return Promise.reject(error);
