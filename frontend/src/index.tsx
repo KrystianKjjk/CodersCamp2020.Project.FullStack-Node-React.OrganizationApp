77b339bcabebb375f44a3748c9123b08692c3202
Krystian <kry.kjjk@op.pl>
2021-05-03 23:34:18 +0200

    add react query

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
-import {ThemeProvider} from '@material-ui/core/styles';
-import theme from "./theme/customMaterialTheme";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
+import * as serviceWorker from './serviceWorker'
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from './theme/customMaterialTheme'
+import { QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import queryClient from './QueryClient'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
