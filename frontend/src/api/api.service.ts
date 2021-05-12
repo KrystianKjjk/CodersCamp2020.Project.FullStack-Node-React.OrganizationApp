ac5a2146379be97baee72a2c59a48321576099a4
robert-czwartosz <robert.czwartosz@tlen.pl>
2021-05-12 19:05:07 +0200

    generic api service

--- frontend/src/api/api.service.ts
+++ frontend/src/api/api.service.ts
@@ -6,25 +6,33 @@ axios.defaults.headers.common = {
 }
 
 const api = {
-  post: (path: string, data: any, config?: AxiosRequestConfig) => {
-    return axios.post(path, data, config)
+  post: <T = any, R = any>(
+    path: string,
+    data: T,
+    config?: AxiosRequestConfig,
+  ) => {
+    return axios.post<R>(path, data, config)
   },
-  get: (path: string, config?: AxiosRequestConfig) => {
-    return axios.get(path, config)
+  get: <R = any>(path: string, config?: AxiosRequestConfig) => {
+    return axios.get<R>(path, config)
   },
-  delete: (path: string, config?: AxiosRequestConfig) => {
-    return axios.delete(path, config)
+  delete: <R = any>(path: string, config?: AxiosRequestConfig) => {
+    return axios.delete<R>(path, config)
   },
-  patch: (path: string, data: any, config?: AxiosRequestConfig) => {
-    return axios.patch(path, data, config)
+  patch: <T = any, R = any>(
+    path: string,
+    data: T,
+    config?: AxiosRequestConfig,
+  ) => {
+    return axios.patch<R>(path, data, config)
   },
-  put: (path: string, data: any) => {
+  put: <T = any, R = any>(path: string, data: T) => {
     const putConfig = {
       headers: {
         'Content-Type': 'application/json;charset=UTF-8',
       },
     }
-    return axios.put(path, data, putConfig)
+    return axios.put<R>(path, data, putConfig)
   },
 }
 
