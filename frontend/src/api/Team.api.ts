8785a13619e1517f508df22c649e1257dda6181a
robert-czwartosz <robert.czwartosz@tlen.pl>
2021-05-14 10:19:52 +0200

    add fixes

--- frontend/src/api/Team.api.ts
+++ frontend/src/api/Team.api.ts
@@ -1,10 +1,33 @@
 import api from './api.service'
 import { TeamData, Team, TeamInfo } from '../models/Team.model'
 import { CourseData } from '../models/Course.model'
-import { calcUserProjectGrade, calcUserTasksGrade, calcUserTestsGrade } from '.'
-import { getTeamProjects } from './TeamProject.api'
-import { User, UserData, userStatusDict, userTypeDict } from '../models'
+import { calcProjectGrade, calcUserProjectGrade, calcUserTasksGrade, calcUserTestsGrade } from '.'
+import { TeamProject, TeamProjectData, User, UserData, userStatusDict, userTypeDict } from '../models'
 import { getMentorSheets } from './Sheet.api'
+import { getProject } from './Project.api'
+
+async function getTeamProjects(id: string, mentorId?: string): Promise<TeamProject[]> {
+  const projectsRes = await api.get(`/teams/${id}/projects`);
+  const projectsData = projectsRes.data as TeamProjectData[];
+  const projects = await Promise.all( projectsData.map( async project => {
+      const parentProject = await getProject(project.parentProjectId);
+      return {
+          id: project._id,
+          name: project.projectName,
+          overallGrade: 0,
+          sectionName: parentProject?.sectionName ?? '---',
+          url: project.projectUrl,
+          description: project.description,
+  }} ) );
+  const grades = await getMentorSheets(mentorId);
+  grades?.forEach(sheet => {
+      const idx = projects.findIndex(project => project.id === sheet.projectID);
+      if(idx > -1){
+          projects[idx].overallGrade = calcProjectGrade(sheet)[0];
+      }
+  })
+  return projects;
+}
 
 export const getTeams = async (): Promise<Team[]> => {
   const activeCourse = localStorage.getItem('activeCourse')
