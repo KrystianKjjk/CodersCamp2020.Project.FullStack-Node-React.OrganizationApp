5c5490e9f2488a2214c2ad1d13a090fb156d9d4b
robert-czwartosz <robert.czwartosz@tlen.pl>
2021-05-14 22:45:21 +0200

    apply generic functions

--- frontend/src/hooks/useQuery/useCourse.ts
+++ frontend/src/hooks/useQuery/useCourse.ts
@@ -1,8 +1,12 @@
 import * as api from '../../api/Course.api'
-import { useQuery } from 'react-query'
+import { useGenericQuery } from './useGenericQuery'
 import queryClient from '../../QueryClient'
 import * as sectionsApi from '../../api/Section.api'
-import { Course, CourseDto, SectionListElement } from '../../models/Course.model'
+import {
+  Course,
+  CourseDto,
+  SectionListElement,
+} from '../../models/Course.model'
 
 const fetchCourseAsync = async (courseId: string): Promise<Course> => {
   const courseResponse = await api.getCourse(courseId)
@@ -15,30 +19,22 @@ const fetchCourseAsync = async (courseId: string): Promise<Course> => {
   }
 }
 
-const useCourse = (courseId: string) => {
-  const { isLoading, error, data, isFetching } = useQuery(
-    ['course', courseId],
-    () => fetchCourseAsync(courseId),
-    {
-      retryOnMount: false,
-      notifyOnChangeProps: ['data', 'error', 'isFetching', 'isLoading'],
-    },
-  )
-  return { isLoading, error, data, isFetching }
-}
-
+const useCourse = (courseId: string) =>
+  useGenericQuery(['course', courseId], fetchCourseAsync(courseId))
 export default useCourse
 
-export const updateCourseAsync =  async (course: Course, sectionsIdToDelete:string[]): Promise<void> => {
-   try{
-    sectionsIdToDelete.forEach(async(sectionId)=>{
-      await sectionsApi.deleteSection(sectionId);
+export const updateCourseAsync = async (
+  course: Course,
+  sectionsIdToDelete: string[],
+): Promise<void> => {
+  try {
+    sectionsIdToDelete.forEach(async (sectionId) => {
+      await sectionsApi.deleteSection(sectionId)
     })
-    await api.updateCourse(course);
+    await api.updateCourse(course)
     await queryClient.refetchQueries(['course', course._id], { active: true })
-    return Promise.resolve();
-   }
-    catch(exception){
-      return Promise.reject(exception);
-    }
-  };
+    return Promise.resolve()
+  } catch (exception) {
+    return Promise.reject(exception)
+  }
+}
