5c6ad620ee42edb162aef4b7f3b57d7ec4597c85
robert-czwartosz <robert.czwartosz@tlen.pl>
2021-05-18 20:45:03 +0200

    repair course tests

--- frontend/src/pages/Admin/CourseCreate/CourseCreate.test.tsx
+++ frontend/src/pages/Admin/CourseCreate/CourseCreate.test.tsx
@@ -4,24 +4,38 @@ import userEvent from '@testing-library/user-event'
 import { Provider } from 'react-redux'
 import { store } from '../../../app/store'
 import CourseCreate from '.'
+import { QueryClientProvider } from 'react-query'
+import queryClient from '../../../QueryClient'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import { Course } from '../../../models'
 
-const mockCreateCourse = jest.fn()
-jest.mock('../../../api/courses.service', () => {
-  return jest
-    .fn()
-    .mockImplementation(() => ({ createCourse: mockCreateCourse }))
-})
+const courses: Course[] = []
+
+let mock = new MockAdapter(axios)
+mock
+  .onPost(`${process.env.REACT_APP_API_URL}/api/courses`)
+  .reply((config) => {
+    courses.push(config.data)
+    return [200, config.data]
+  })
+
+mock.onGet(`${process.env.REACT_APP_API_URL}/api/courses`).reply(200, courses)
+
+mock.onAny().reply(200)
 
 describe('CourseCreator', () => {
   it('should disable submit when course name is empty', () => {
     render(
-      <Provider store={store}>
-        <CourseCreate />
-      </Provider>,
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <CourseCreate />
+        </Provider>
+      </QueryClientProvider>
     )
 
     userEvent.click(screen.getByText('SAVE'))
 
-    expect(mockCreateCourse).not.toHaveBeenCalled()
+    expect(courses).toHaveLength(0)
   })
 })
