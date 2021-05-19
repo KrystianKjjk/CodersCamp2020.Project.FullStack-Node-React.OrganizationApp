50f861d8291334ecca407ee4d25c69674ea9f000
ireneusz <ireneusz.petryszak@gmail.com>
2021-05-19 16:07:29 +0200

    added menu change

--- frontend/src/components/MenuSmall/MenuSmall.tsx
+++ frontend/src/components/MenuSmall/MenuSmall.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import styles from './MenuSmall.module.css';
 
 export interface MenuSmallProps {
-  name: string;
 }
 
 const MenuSmall: React.FC< MenuSmallProps > = props => {
   return (
     <div>
-      {props.name}
+      elo
     </div>
   );
 };
